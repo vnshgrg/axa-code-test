1f88bf585b164db06cf218450ce3968aa1868457
Avinash <ghale.avinash@gmail.com>
2023-07-11 01:19:30 +0900

    changed cron interval

--- src/server/server.ts
+++ src/server/server.ts
@@ -14,7 +14,7 @@ app.use("/api/message", messageRouter);
 // set up our simple interval based cron job
 cronJob(async () => {
   processEmails();
-}, 10 * 1000); // runs every 15 minutes
+}, 15 * 1000); // runs every 15 seconds
 
 const port = environment.app_port || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
